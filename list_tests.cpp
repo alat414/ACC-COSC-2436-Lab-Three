f01235d9067e9bb2cb96512c796327650b3b6102
alat414 <gustavo.alatriste@g.austincc.edu>
2025-11-16 19:17:54 -0600

    omitting tests for arraylist, adding tests including insert, getentry for link list

--- list_tests.cpp
+++ list_tests.cpp
@@ -7,7 +7,7 @@
 // ============================================================================
 // PART 1: List ADT Unit Tests
 // ============================================================================
-
+/*
 TEST_CASE("Simple Integer ArrayList")
 {
     ArrayList<int, 64> list;
@@ -35,40 +35,19 @@ TEST_CASE("Simple Integer ArrayList")
         CHECK(list.isEmpty());
     }
 }
-/*
-TEST_SUITE("List Tests")
+*/
+TEST_CASE("Link List Implementation")
 {
-    TEST_CASE("ArrayList Implementation") 
+    LinkedList<int> List;
+
+    SUBCASE("Verifying the inser of the Link List")
     {
-        ArrayList<int, 64> list;
+        CHECK(List.insert(1,8));
+        CHECK(List.insert(2,3));
+        CHECK(List.getEntry(2)==3);
+    }
+}
 
-        std::cout << "DEBUG: isEmpty function returns " << list.isEmpty() << std::endl;
-        std::cout << "DEBUG: getLength function returns " << list.getLength() << std::endl;
-
-
-        SUBCASE("Empty list")
-        {
-            CHECK(list.isEmpty() == true);
-            CHECK(list.getLength() == 0);
-        }
-
-        SUBCASE("Access empty list throws")
-        {
-            CHECK_THROWS_AS(list.getEntry(0), std::out_of_range);
-            CHECK_THROWS_AS(list.remove(0), std::out_of_range);
-        }
-        SUBCASE("Add item at the beginning")
-        {
-            list.insert(0,10);
-            CHECK(list.getEntry(0) == 10);
-        }
-        
-    
-
-    TEST_CASE("LinkedList Implementation") 
-    {
-        LinkedList<int> list;
-    }*/
 
 
 // ============================================================================
